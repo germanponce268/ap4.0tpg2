17973bb5f11c4d4bc9c6a81bbda2fc565e8d6922
German Ponce <germanponce268@gmail.com>
2023-03-31 22:22:13 -0300

    update

--- src/main/java/Prode/Fase.java
+++ src/main/java/Prode/Fase.java
@@ -1,18 +1,29 @@
 package Prode;
 
+import java.util.Collection;
 import java.util.HashMap;
 
 public class Fase {
     private Integer nroFase;
+    private HashMap<Integer,HashMap<Integer,Partido>>partidosfases;
     private HashMap<Integer, Partido> partidos;
 
     public Fase() {
         this.partidos = new HashMap<>();
+        this.partidosfases = new HashMap<>();
     }
 
     public void agregarPartido(Integer fechaNro, Partido partido){
         this.partidos.put(fechaNro, partido);
     }
+
+    public void agregarFasePartido(Integer fase, HashMap fasePartido){
+        this.partidosfases.put(fase, fasePartido);
+    }
+    public HashMap<Integer, HashMap<Integer, Partido>> getPartidosfases() {
+        return partidosfases;
+    }
+
     public HashMap<Integer, Partido> getPartidos() {
         return partidos;
     }
@@ -24,6 +35,13 @@ public class Fase {
         } else  {
             return 2;
         }
+    }
 
+    public Integer getNroFase() {
+        return nroFase;
+    }
+
+    public void setNroFase(Integer nroFase) {
+        this.nroFase = nroFase;
     }
 }
--- src/main/java/Prode/Main.java
+++ src/main/java/Prode/Main.java
@@ -10,22 +10,27 @@ public class Main {
         static Fase fase = new Fase();
         static TargetaProde prode = new TargetaProde();
         static Apuesta jugada = new Apuesta() ;
-        static Persona jugador = new Persona("Juan", "Perez");
+        static Persona jugador = new Persona("Jose", "Perez");
         static int nro = 0;
     public static void main(String[] args) throws IOException {
-        Path archivoResultados = Paths.get("src/resultados.txt");
-        Path archivoPronostioco = Paths.get("src/pronostico.txt");
+        Path archivoResultados = Paths.get("src/resultados2.txt");
+        Path archivoPronostioco = Paths.get("src/apuestas2.txt");
+        Path resultadosMundial = Paths.get("src/partidosmundial2.csv");
+        Path pronosticosMundial = Paths.get("src/pronosticosMundial.csv");
 
-        Scanner lector = new Scanner(archivoResultados);
-        Scanner lector2 = new Scanner(archivoPronostioco);
+        Scanner lector = new Scanner(resultadosMundial);
+        Scanner lector2 = new Scanner(pronosticosMundial);
 
 
-        lector.useDelimiter("[;|\\r|\\n|]");
-        lector2.useDelimiter("[,|\\r|\\n|]");
+        lector.useDelimiter("[;|\\r|\\n]");
+        lector2.useDelimiter("[;\\r|\\n|]");
 
         while(lector.hasNext()){
 
             //se leen los valores de resultados y se almacenan en variables
+        String nroPartido = lector.next();
+        if(nroPartido.equals("") || nroPartido.equals(" ")) nroPartido = lector.next();
+        int nroFase = lector.nextInt();
         String local = lector.next();
         if(local.equals("")||local.equals(" ")) local = lector.next();
         String visitante = lector.next();
@@ -38,10 +43,15 @@ public class Main {
 
         //se almacena el partido en una coleccion de tipo HashMap a traves del metodo agregarPartido()
         fase.agregarPartido(partido.getNro(), partido);
-
+        fase.setNroFase(nroFase);
+        fase.agregarFasePartido(nroFase, fase.getPartidos());
         //se leen y almacenan en variables los valores de la apuesta
+        //String nombre = lector2.next();
+        String fecha = lector2.next();
+        if(fecha.equals("") || fecha.equals(" ")) fecha = lector2.next();
+        int ronda = lector2.nextInt();
         String equipo1 = lector2.next();
-            if(equipo1.equals("")||local.equals(" ")) equipo1 = lector2.next();
+        if(equipo1.equals("")||local.equals(" ")) equipo1 = lector2.next();
         String equipo2 = lector2.next();
         int apuesta = lector2.nextInt();
 
@@ -50,9 +60,10 @@ public class Main {
 
         //se agrega la jugada al ArrayList de jugadas
         prode.agregarApuesta(jugada);
+        System.out.println(partido);
         }
 
-        //se calcula el puntaje de la apuesta realizada
+       ///se calcula el puntaje de la apuesta realizada
         prode.calcularPuntaje(fase.getPartidos().values());
         jugador.setTargetaProde(prode);
 
@@ -60,4 +71,6 @@ public class Main {
 
         JOptionPane.showMessageDialog(null,"El jugador" + jugador.getNombre() + " saco " + prode.getPuntos()+ " puntos!") ;
     }
+
+
 }
