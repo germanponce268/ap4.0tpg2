bb7c67d8b2975605f0090396ee2cef2b2b240d16
German Ponce <germanponce268@gmail.com>
2023-04-21 17:02:34 -0300

    commit

--- src/main/java/Prode/TargetaProde.java
+++ src/main/java/Prode/TargetaProde.java
@@ -11,11 +11,11 @@ public class TargetaProde {
     private HashMap<Integer, Integer> puntosFases;
     private ArrayList<Apuesta> apuestas;
     private HashMap<String, Integer> participantes;
-    private Integer contadorFases;
+    private Integer puntosGanados;
 
     public TargetaProde() {
         this.apuestas = new ArrayList<>();
-        this.contadorFases = 0;
+        this.puntosGanados = 0;
         this.participantes = new HashMap<>();
         this.puntosFases = new HashMap<>();
     }
@@ -29,18 +29,23 @@ public class TargetaProde {
         Integer nroFase = apuesta.getFase();
         Integer nroPartidoApuesta = apuesta.getNroPartido();
         String nombre = apuesta.getNombreApostador();
-       for(Partido partido : partidos){
+
+        for(Partido partido : partidos){
            Integer nroPartido = partido.getNro();
            Integer resultado = partido.getResultado();
+
            if(nroPartidoApuesta.equals(nroPartido)){
+
                if(apuestaValor.equals(resultado)){
-                   
-                   this.contadorFases = 1 ;
+
+                   this.puntosGanados = 1 ;
+
                    Integer ultimoPuntaje = this.participantes.get(nombre);
+
                    if(ultimoPuntaje == null){
-                       this.participantes.put(apuesta.getNombreApostador(), this.contadorFases);
+                       this.participantes.put(apuesta.getNombreApostador(), this.puntosGanados);
                    }else{
-                       this.participantes.put(apuesta.getNombreApostador(), this.contadorFases + ultimoPuntaje);
+                       this.participantes.put(apuesta.getNombreApostador(), this.puntosGanados + ultimoPuntaje);
                    }
                }
            }
